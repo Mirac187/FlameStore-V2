e22050b187e4fa3674ab0d76eba135650e478e00
DarkFlameOfficial <mustafamirac000@gmail.com>
2026-08-29 08:57:00 +0300

    main.js

--- main.js
+++
@@ -1,2 +0,0 @@
-import {createApp} from 'vue'; import App from './App.vue'; import router from './router'; import './style.css';
-const app=createApp(App); app.use(router); app.mount('#app'); if('serviceWorker' in navigator) window.addEventListener('load',()=>navigator.serviceWorker.register('/sw.js').catch(console.warn));
